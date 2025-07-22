47b8bcb98d00f0bd891cfde54dec033c33a18e8a
Shem (RetroBreeze) <retrobreezeyt@gmail.com>
2025-07-22 14:36:19 +0100

    Clip icon reflections (#31)

--- app/src/main/java/com/retrobreeze/ribbonlauncher/ReflectiveGameIcon.kt
+++ app/src/main/java/com/retrobreeze/ribbonlauncher/ReflectiveGameIcon.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -38,6 +40,7 @@ fun ReflectiveGameIcon(
             contentDescription = null,
             modifier = Modifier
                 .fillMaxSize()
+                .clip(RoundedCornerShape(12.dp))
                 .graphicsLayer {
                     scaleY = -1f
                 },
